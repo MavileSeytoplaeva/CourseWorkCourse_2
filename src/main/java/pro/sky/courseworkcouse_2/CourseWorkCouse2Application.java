99a907dbfa0fce65541bb06bb050580b91b4cd66
Мавиле Сейтоплаева <mavile.seitoplaeva@gmail.com>
2023-07-24 21:37:02 +0300

    Init

---
+++ src/main/java/pro/sky/courseworkcouse_2/CourseWorkCouse2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.courseworkcouse_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkCouse2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorkCouse2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/courseworkcouse_2/CourseWorkCouse2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.courseworkcouse_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkCouse2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
